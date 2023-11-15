6fda44cdd330b6793ccebbb88d7d99ca7a8f2cfe
Masirika2 <91177805+Masirika2@users.noreply.github.com>
2023-11-16 02:14:11 +0300

    Update models.js

--- models.js
+++ models.js
@@ -1,27 +1,26 @@
 const mongoose = require('mongoose');
 
-// Citizen schema
+// Citizen Schema
 const citizenSchema = new mongoose.Schema({
   name: String,
-  dob: String,
+  dob: Date,
   fatherName: String,
   motherName: String,
   gender: String,
-  bloodGroup: String
+  bloodGroup: String,
 });
 
-const Citizen = mongoose.model('Citizen', citizenSchema);
-
-// Title schema
+// Title Schema
 const titleSchema = new mongoose.Schema({
   ownerName: String,
   location: String,
   size: String,
   coordinates: String,
   titleNumber: String,
-  photo: String
+  photo: String,
 });
 
+const Citizen = mongoose.model('Citizen', citizenSchema);
 const Title = mongoose.model('Title', titleSchema);
 
 module.exports = { Citizen, Title };
